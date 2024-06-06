dc8575497743d76ce139cb394023af22ee3176be
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-06-06 10:15:08 +0200

    Styling

---
+++ 2-vue-styling/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const todo = { text: "Lorem ipsum", done: false };
+</script>
+
+<template>
+  <span :class="todo.done ? 'mytext' : ''">{{ todo.text }}</span>
+</template>
+
+<style scoped lang="scss">
+.mytext {
+  color: var(--vt-c-indigo);
+  text-decoration: line-through;
+}
+</style>
---
+++ 2-vue-styling/src/components/Wrapper.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper"></div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+}
+</style>
